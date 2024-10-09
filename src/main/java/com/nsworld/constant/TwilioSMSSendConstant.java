d5d6009dc57dd9aab1c2d18f19186f6062d1ff63
unknown <namo.shelke92@gmail.com>
2024-10-09 11:59:28 +0530

    code commited

--- src/main/java/com/nsworld/constant/TwilioSMSSendConstant.java
+++ src/main/java/com/nsworld/constant/TwilioSMSSendConstant.java
@@ -1,10 +1,7 @@
 package com.nsworld.constant;
 
 public class TwilioSMSSendConstant {
-//    #SMS send using Twilio
-//    twilio.account.sid=AC47b086d28023052316a0e306b7368f75
-//    twilio.auth.token=c9e3d6388163921798e6a32ae8c30eda
-//    twilio.phone.number=+13158733843
+
     public static final  String USER_SUBJECT = "Welcome to NSWorld platform!";
     public static final   String USER_BODY = "Dear ";
     public static final   String USER_BODY_MSG= ",\n\nThank you for registering with us.we have 10 years experty to teach you\n\nBest regards,\nNSWorld Pvt.Ltd";
